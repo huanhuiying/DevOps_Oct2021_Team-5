f8a12bbabea0ab05226123e4b5fd0b8e80c67cf0
Jing Ting Megan Png <s10196822@connect.np.edu.sg>
2021-11-30 12:04:55 +0800

    add exit func & copy hui ying menu code into func

---
+++ exit.py
@@ -0,0 +1,68 @@
+#Exit Game to Menu
+
+#US20 - As a user, I want to be able to go back to the main menu after I have ended the game.
+
+
+#B is a bool, will trigger print menu if Bool is true
+def exitToMenu(b):
+    if(b):
+        print_menu()
+
+#Made Hui Ying's code into a function  
+def print_menu():
+    turn_counter = 0
+    main_menu_options = ["Start new game", "Load saved game"]
+    configure_menu_options = ["Build a HSE", "Build a BCH", "See remaining buildings", "See current score"]
+
+    def main_menu(main_menu_options):
+        for i in range (len(main_menu_options)):
+            print("{}. {}".format(i+1,main_menu_options[i]))
+            print()
+
+    def configure_menu(configure_menu_options):
+        for i in range (len(configure_menu_options)):
+            print("{}. {}".format(i+1,configure_menu_options[i]))
+            print()
+
+    print("Welcome, mayor of Simp City")
+    print("----------------------------")
+    main_menu(main_menu_options)
+    print("{}. {}".format("0", "Exit"))
+    choosen_menu_option =  int(input("Your choice? "))
+    print()
+
+    #Start new game
+    if choosen_menu_option == 1:
+        print("{} {}" .format("Turn", str(turn_counter+1)))
+        #code to print the sqaure buildings here
+        while True:
+            configure_menu(configure_menu_options)
+            print()
+            print("{}. {}".format("5", "Save game"))
+            print("{}. {}".format("0", "Exit to main menu"))
+            choosen_configureMenu_option =  int(input("Your choice? "))
+            print()
+            if choosen_configureMenu_option == 1:
+                turn_counter = turn_counter+1 #can do in the function
+                input("{} ".format("Build where?"))
+                print("{} {}" .format("Turn", str(turn_counter+1)))
+                #code of the function that auto-generates the housing selections and plant the building selected to the inputted location 
+                print()
+            if choosen_configureMenu_option == 2:
+                turn_counter = turn_counter+1# can do in the function
+                input("{} ".format("Build where?"))
+                print("{} {}" .format("Turn", str(turn_counter+1)))
+                #code of the function that auto-generates the housing selections and plant the building selected to the inputted location 
+                print()
+            if choosen_configureMenu_option == 3:
+                #code of the function that allows the player to see the remanining building
+                print()
+            if choosen_configureMenu_option == 4:
+                #code of the function that allows the player to check their current score
+                print()
+            if choosen_configureMenu_option == 5:
+                #code of the function that allows the player to save their current game progress
+                print()
+            else:
+                #code of the function that allows the player to exit to main menu
+                print()
