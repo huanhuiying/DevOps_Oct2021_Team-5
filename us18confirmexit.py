47f2104b74a31446d9c6851bea24f10d68c33769
Jing Ting Megan Png <s10196822@connect.np.edu.sg>
2022-01-02 21:08:45 +0800

    edited us18 file by commenting out code

--- us18confirmexit.py
+++ us18confirmexit.py
@@ -1,9 +1,9 @@
-def exitToMenu():
-    return
+#def exitToMenu():
+    #return
 
-def printPrompt():
-    print("Invalid input. Please enter 'Y' or 'N' to continue. ")
-    return
+#def printPrompt():
+    #print("Invalid input. Please enter 'Y' or 'N' to continue. ")
+    #return
 
-def checkUserInput(input):
-    return
+#def checkUserInput(input):
+    #return
