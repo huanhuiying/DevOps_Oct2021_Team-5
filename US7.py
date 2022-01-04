50bd6b373536416ce5cdd0a36992fb9a00c46690
claris <claristoh0911@gmail.com>
2022-01-04 23:33:50 +0800

    TDD done, not all tests passed

---
+++ US7.py
@@ -0,0 +1,6 @@
+def printBuildingCfm():
+    return ("Confirm using a building [Y/N]: ")
+
+
+def checkUserInput(input):
+    return
---
+++ test_US7.py
@@ -0,0 +1,17 @@
+import pytest
+from US7 import *
+
+def test_buildingConfirm():
+    result = printBuildingCfm()
+    assert result == "Y" or "N"
+
+# def test_buildingConfirm_NoInput():
+#     result = opt1()
+#     assert result == "Invalid input. Confirm using building [Y/N]: "
+
+@pytest.mark.parametrize("input, expectedResult",
+[("Y", "Building used"),("N", "Not used")])
+
+def test_buildingConfirm_userInput(input, expectedResult):
+    result = checkUserInput(input)
+    assert result == expectedResult
