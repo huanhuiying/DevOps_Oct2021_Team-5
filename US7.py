986654f70f839fdfbdad2ae005dad1b551c43860
claris <93189702+claris0911@users.noreply.github.com>
2022-01-18 00:52:44 +0800

    cleaned up US7 code

--- US7.py
+++ US7.py
@@ -1,22 +1,4 @@
 
-
-# def buildingConfirm1(cfm):
-#     while True:
-#         cfm = input("Y/N: ")
-#         try:
-#             cfm = cfm.capitalize()
-#             if cfm == "Y" or cfm == "N":
-#                 if cfm == "Y":
-#                     print("Building confirmed")
-#                 else:
-#                     print("Cancelled")
-#                 break
-#             else:
-#                 print("Use only 'Y' or 'N' to confirm")   
-#         except ValueError:
-#             print("Use only 'Y' or 'N' to confirm:<")
-#             continue
-
 def buildingConfirm(cfm):
     try: 
         if cfm == "Y" or cfm == "N" or cfm == "y" or cfm == "n":
@@ -34,6 +16,3 @@ def buildingConfirm(cfm):
         return (True)
 
 
-#cfm = 6
-#cfm = str(input("Y/N: "))
-#buildingConfirm(cfm)
