4ea176aa8478061cbd67449686492b7f760b230c
amanda <s10196608@connect.np.edu.sg>
2022-01-17 23:16:24 +0800

    All tests has passed for US6 and US12 function

--- US6and12.py
+++ US6and12.py
@@ -2,10 +2,12 @@ import re
 
 def location_format(location):
     choice_regex = '^([a-d]|[A-D])[1-4]$'
-
-    location = location[0] + location[1]
-    
-    if(re.search(choice_regex, location)):
-        return True
-    else:
+    try:
+        location = location[0] + location[1]
+        
+        if(re.search(choice_regex, location)):
+            return True
+        else:
+            return False
+    except:
         return False
