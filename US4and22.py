074da164a729ee528001580d1e1d73bd1058066c
claris <claristoh0911@gmail.com>
2022-01-24 00:02:35 +0800

    commit  changes

---
+++ US4and22.py
@@ -0,0 +1,6 @@
+# US4 - As a user, I want to be able to select the "Exit"
+#  option from the main menu so that I can end the program.
+
+# US22 - As a user, I want to be shown a prompt to confirm my 
+# choice to end the program so I don't accidentally end the program. 
+
