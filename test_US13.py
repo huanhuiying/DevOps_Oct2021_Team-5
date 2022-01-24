6a7e29ef3fcae8ef301aa517977fb7e2af70d6d5
claris <claristoh0911@gmail.com>
2022-01-25 00:49:41 +0800

    US13 unit test done, no tests passed

--- test_US13.py
+++ test_US13.py
@@ -5,12 +5,12 @@ from US13 import *
 #  for choices so that I do not get confused.
 
 
-@pytest.mark.parametrize("choosen_configureMenu_option, expectedResult",
-[(1, False), (3, False),(6, True)])
+@pytest.mark.parametrize("option, expectedResult",
+[(1, False),(2, False),(3, False),(4, False),(5, False),
+(7,True),(0, False),("s",True)])
 
-
-def test_configMenuVal(choosen_configureMenu_option, expectedResult):
-    result = configMenuVal(choosen_configureMenu_option)
+def test_configMenuVal_breakReturn(option, expectedResult):
+    result = configMenuVal(option)
     assert result == expectedResult
    
 
